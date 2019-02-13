3433fb91ac60ecf58182f6cc9c754f41fc9588c8
dila93 <sebas.dila@gmail.com>
2019-02-13 13:37:28 -0500

    mongodb atlas

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	port: process.env.PORT || 3000,
-	db: process.env.MONGODB || 'mongodb://localhost:27017/portafilio',
+	db: process.env.MONGODB || 'mongodb+srv://dila93:dila93@portafolio-dila-test-xvksx.mongodb.net/test?retryWrites=true',
 
 	develop: {
         name: 'develop',
